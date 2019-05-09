a1bfc784bf393b52c5d618dd62b29ec89dffdbd5
Jessie Tizon <jct2418@gmail.com>
2019-05-09 17:26:48 -0600

    Second Commit

---
+++ src/composition/Split.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Split(props) {
+    const combinedClassName = `split ${props.className}`;
+    return (
+      <div className={combinedClassName}>
+        {props.children}
+      </div>
+    );
+  }
+
+export default Split;
