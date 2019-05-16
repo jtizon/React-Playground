ce119d8661d41fb297519c60eac3a74d71398a1d
Jessie Tizon <jct2418@gmail.com>
2019-05-16 12:55:09 -0600

    Test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Split from './composition/Split';
 import './App.css';
 import Tooltip from './composition/Tooltip';
 
+// before
 const firstTooltip = (
   <Tooltip message='tooltip message'>
    ipsum
@@ -13,12 +14,23 @@ const secondTooltip = (
     officiis
   </Tooltip>
 )
+// after
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
     <main className='App'>
       <Split className='left' flexBasis='2'>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
         <Tooltip message='one more tooltip message'>
           Necessitatibus?
         </Tooltip>
--- src/composition/Tooltip.js
+++ src/composition/Tooltip.js
@@ -12,6 +12,28 @@ function Tooltip(props) {
         </div>
       </span>
     )
+}
+class TooltipClass extends React.Component {
+    static defaultProps = {
+        color: '#01A800', 
+    };
+    render() {
+      console.log('Using a class component!')
+      console.log(this.props)
+      return (
+        <span className='Tooltip'>
+          <span
+            className='Tooltip-content'
+            style={{ color: this.props.color }}
+          >
+            {this.props.children}
+          </span>
+          <div className='Tooltip-message'>
+            {this.props.message}
+          </div>
+        </span>
+      )
+    }
   }
-
 export default Tooltip;
+export default TooltipClass;
